179fb4c772c3466ccbaf54ec25356e9beb824230
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-06-26 16:48:40 -0700

    0

--- conversion.c
+++ conversion.c
@@ -16,10 +16,6 @@ int handle_conversion(const char *format, va_list args)
 	}
 	else
 	{
-		if (*format == 'c')
-			count += print_char(va_arg(args, int));
-		else if (*format == 's')
-			count += print_strings(va_arg(args, char*));
 		else if (*format == 'S')
 			count += print_nonprintable(va_arg(args, char*));
 		else if (*format == 'd' || *format == 'i')
