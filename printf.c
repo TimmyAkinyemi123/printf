653bb066b99b4b288a4d257023053df012a5d29d
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-06-27 10:39:03 -0700

    0

--- printf.c
+++ printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (!format || format[0] == '%' && !format[1])
+	if (!format || format == '%' && !format++)
 		return (-1);
 	while (*format)
 	{
